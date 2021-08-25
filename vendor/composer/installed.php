fd83918b48eaa7f8e90f94e14040a6d0e9b3d157
alex.dos <leonedimars@gmail.com>
2021-08-25 17:10:27 +0200

    Test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '4a36bc3b77a878ce06a606c37c16d4d90c4945a1',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4a36bc3b77a878ce06a606c37c16d4d90c4945a1',
+    ),
+  ),
+);
