b90718adf21d3cdba16dd2e21a81b65f31c59823
alex.dos <leonedimars@gmail.com>
2021-09-09 14:38:01 +0200

    Initialize db

--- app/db/deleteDb.php
+++ app/db/deleteDb.php
@@ -4,6 +4,8 @@
 
 require 'connDb.php';
 
-$pdo->exec("DROP TABLE users");
-
+$pdo->exec("SET FOREIGN_KEY_CHECKS = 0"); // Unlink Foreign Key
+$pdo->exec("DROP TABLE user");
+$pdo->exec("DROP TABLE post");
+$pdo->exec("SET FOREIGN_KEY_CHECKS = 1"); // Link Foreign Key
 echo 'Database TABLES has been deleted';
